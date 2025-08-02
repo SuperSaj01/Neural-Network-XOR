a50e727aeea504850b23b5a60e3d13d65452c3c9
SuperSaj01 <saj.kha01@gmail.com>
2025-08-02 02:17:45 +0100

    Feed Forward
    -Calculating each node outputs and sending it to the next layer

--- source/program.cpp
+++ source/program.cpp
@@ -16,7 +16,7 @@
 #include <vector>
 #include <random>
 
-double sigmoid(double input)
+static double sigmoid(double input)
 {
     return 1.0 / (1.0 + exp(-input));
 };
@@ -24,7 +24,7 @@ double sigmoid(double input)
 class Layer
 {
 private:
-    int nodeInput;
+    int nodesInput;
     int nodesOutput;
     std::vector<std::vector<double>> weights;
     std::vector<double> biases;
@@ -32,32 +32,70 @@ private:
 public:
     Layer(int nodeInput, int nodesOutput);
     void InitialiseWeightsAndBiases();
+   
+    std::vector<double> CalculateOutput(std::vector<double> Inputs);
+  
 };
 
 
 void Layer::InitialiseWeightsAndBiases()
 {
+   // std::cout << "nodes going in" << nodesInput << " " << "nodes going out: " << nodesOutput << std::endl;
+
     biases.resize(nodesOutput, 0.0);
 
-    weights.resize(nodeInput, std::vector<double>(nodesOutput));
+    weights.resize(nodesInput, std::vector<double>(nodesOutput));
 
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::normal_distribution<> dist(0, std::sqrt(2.0 / nodeInput));
+    std::normal_distribution<> dist(0, std::sqrt(2.0 / nodesInput));
 
-    double randomNum = ((double)std::rand() / RAND_MAX);
+    
 
-    for (int i = 0; i < nodeInput; i++)
+    for (int i = 0; i < nodesInput; i++)
     {
         for (int j = 0; j < nodesOutput; j++)
         {
+            double randomNum = ((double)std::rand() / RAND_MAX);
             weights[i][j] = randomNum;
         }
     }    
     
 }
 
-Layer::Layer(int nodeInput, int nodesOutput) : nodeInput(nodeInput), nodesOutput(nodesOutput)
+
+std::vector<double> Layer::CalculateOutput(std::vector<double> Inputs)
+{
+    std::cout << "nodes going in: " << nodesInput << " " << "nodes going out: " << nodesOutput << std::endl;
+
+   
+    std::vector<double> outputs;
+    double output = 0.00;
+    for (int currentOutputNode = 0; currentOutputNode < nodesOutput; currentOutputNode++)
+    {
+        for (int currentInputNode = 0; currentInputNode < nodesInput; currentInputNode++)
+        {
+            std::cout << "weights: [" 
+                << currentInputNode 
+                << "] ["
+                << currentOutputNode
+                << "] "
+                << weights[currentInputNode][currentOutputNode] << std::endl;
+            output += weights[currentInputNode][currentOutputNode] * Inputs[0];
+        }
+        output += biases[currentOutputNode];
+        output = sigmoid(output);
+        outputs.push_back(output);
+    }
+
+    return outputs; 
+    
+
+    // std::cout << biases[1]; Something going wrong here 
+}
+
+
+Layer::Layer(int nodesInput, int nodesOutput) : nodesInput(nodesInput), nodesOutput(nodesOutput)
 {
     InitialiseWeightsAndBiases();
 }
@@ -66,11 +104,11 @@ class NeuralNetwork
 {
 private:
     std::vector<int>  NumberOfNeuronsInEachLayer;
-    std::vector<Layer> layers;
     void SetUpLayers(std::vector<int> NumberOfNeuronsInEachLayer);
 
 public:
     NeuralNetwork(const std::vector<int>& NumberOfNeuronsInEachLayer);
+    std::vector<Layer> layers;
 };
 
 NeuralNetwork::NeuralNetwork(const std::vector<int>& NumberOfNeuronsInEachLayer) : NumberOfNeuronsInEachLayer(NumberOfNeuronsInEachLayer)
@@ -89,9 +127,20 @@ void NeuralNetwork::SetUpLayers(std::vector<int> NumberOfNeuronsInEachLayer)
 int main()
 {
     std::vector<int> neuronList = { 2, 3, 1 };
+    std::vector<double> inputLayer = { 1, 1 };
 
     NeuralNetwork nn(neuronList);
    
+    for (Layer layer : nn.layers)
+    {
+        inputLayer = layer.CalculateOutput(inputLayer);
+    }
+
+    for (double num : inputLayer)
+    {
+        std::cout << num;
+    }
+    
 }
 
 
